0d9a5246cf5036c09a0a36ee1f27bb124e9a7264
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2024-12-28 02:11:21 +0500

    type mappings junk

--- generator/ast.sh
+++ generator/ast.sh
@@ -16,6 +16,16 @@ deno  --allow-write ast.ts
 deno --allow-read --allow-write gen.ts
 unavailable_list=`comm -13 available-list.txt extern-list.txt`
 unavailable_count=`echo "$unavailable_list" | wc -l | xargs`
-echo "\nFollowing $unavailable_count functions are not included in the CTGUI.h:\n\n$unavailable_list"
+# echo "\nFollowing $unavailable_count functions are not included in the CTGUI.h:\n\n$unavailable_list"
 
 rm available-list.txt extern-list.txt
+
+# echo "#include <CTGUI/Backend/CSFML-Graphics.h>
+# #include <CTGUI/CTGUI.h>" | 
+# clang \
+#   -E -DCTGUI_EXPORTS -DTGUI_DYNAMIC -DTGUI_NO_DEPRECATED_WARNINGS \
+#   -I../build/_deps/csfml-src/include \
+#   -I../build/_deps/ctgui-src/include \
+#   -I../build/_deps/ctgui-src/src \
+#   -I../build/_deps/tgui-src/include \
+#   -I../build/_deps/sfml-src/include - > merged.c
--- generator/ast.ts
+++ generator/ast.ts
@@ -1,8 +1,49 @@
+import predefinedTypeMaps from "./type-map-pre.json" with { type: "json" };
+
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
 
-const functions = [];
-const structs = [];
+let ast: Ast;
+
+const functions: string[] = [];
+const structs: string[] = [];
+
+const functionDeclarations = [];
+
+function getNativeType(resultType: string): any {
+  if (predefinedTypeMaps[resultType]) {
+    return predefinedTypeMaps[resultType];
+  }
+
+  if (resultType.includes("*")) {
+    return "pointer";
+  }
+
+  if (resultType !== "void") {
+    const nodeIdx = ast.inner.findIndex((node) => node.name === resultType);
+    const node = ast.inner[nodeIdx];
+
+    if (node?.type.qualType.startsWith("enum")) {
+      return getNativeType("int");
+    }
+
+    if (node?.type.qualType.startsWith("struct")) {
+      return ast.inner[nodeIdx - 1].inner
+        .filter((inner) => inner.kind === "FieldDecl")
+        .map((inner) => ({
+          name: inner.name,
+          type: inner.type?.desugaredQualType || inner.type!.qualType,
+          nativeType: getNativeType(
+            inner.type!.qualType||inner.type!.desugaredQualType 
+          ),
+        }));
+    }
+  }
+
+  return resultType;
+}
+
+const typeMap = new Map();
 
 async function processAst() {
   const chunks = [];
@@ -11,7 +52,7 @@ async function processAst() {
   }
 
   try {
-    const ast = JSON.parse(chunks.join(""));
+    ast = JSON.parse(chunks.join(""));
 
     let count = 0;
 
@@ -24,8 +65,35 @@ async function processAst() {
       ) {
         count++;
 
-        if (node.kind === "FunctionDecl") functions.push(node.name);
-        else structs.push(node.name);
+        if (node.kind === "FunctionDecl") {
+          functions.push(node.name);
+
+          const returnType = node.type.qualType.split(/\(.*\)/)[0].trim();
+
+          typeMap.set(returnType, getNativeType(returnType));
+
+          const parameters = node.inner
+            .filter((inner) => inner.kind === "ParmVarDecl")
+            .map((inner) => {
+              const type = inner.type!.qualType.split(/\(.*\)/)[0].trim();
+              typeMap.set(type, getNativeType(type));
+
+              return {
+                name: inner.name,
+                nativeType: getNativeType(type),
+                type,
+              };
+            });
+
+          functionDeclarations.push({
+            name: node.name,
+            result: getNativeType(returnType),
+            returnType,
+            parameters,
+          });
+        } else {
+          structs.push(node.name);
+        }
       }
     });
 
@@ -37,9 +105,6 @@ async function processAst() {
 
     console.log("Available Functions: ", functions.length);
     console.log("Available Structs: ", structs.length);
-
-    // Customize processing here:
-    // console.log(JSON.stringify(ast, null, 2));
   } catch (error) {
     console.error("Failed to parse JSON:", error);
     Deno.exit(1);
@@ -47,3 +112,75 @@ async function processAst() {
 }
 
 await processAst();
+
+Deno.writeFileSync(
+  "function-declarations.json",
+  encoder.encode(JSON.stringify(functionDeclarations, null, 2))
+);
+
+Deno.writeFileSync(
+  "type-map.json",
+  encoder.encode(
+    JSON.stringify(
+      Object.fromEntries([...typeMap.entries()].toSorted()),
+      null,
+      2
+    )
+  )
+);
+
+interface Ast {
+  inner: Node[];
+}
+
+interface Node {
+  id: string;
+  kind: string;
+  loc: Location;
+  range: {
+    begin: RangePoint;
+    end: RangePoint;
+  };
+  name: string;
+  mangledName: string;
+  type: {
+    qualType: string;
+  };
+  storageClass: string;
+  inner: InnerElement[];
+}
+
+interface Location {
+  offset: number;
+  line: number;
+  presumedLine: number;
+  col: number;
+  tokLen: number;
+  includedFrom: {
+    file: string;
+  };
+}
+
+interface RangePoint {
+  offset: number;
+  col: number;
+  tokLen: number;
+  includedFrom: {
+    file: string;
+  };
+}
+
+interface InnerElement {
+  id: string;
+  kind: string;
+  loc?: Location;
+  range: {
+    begin: RangePoint;
+    end: RangePoint;
+  };
+  name?: string;
+  type?: {
+    desugaredQualType: string;
+    qualType: string;
+  };
+}
