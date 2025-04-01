4a358ea0a210454bc3e96b6516aa1161e31e84ab
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2025-04-01 15:26:51 +0500

    feat: Add setup script

---
+++ src/setup.ts
@@ -0,0 +1,24 @@
+const VERSION = [0, 0, 10];
+
+function getBinaryName() {
+  switch (Deno.build.os) {
+    case "darwin":
+      return Deno.build.arch === "aarch64"
+        ? "libctgui-ext.dylib"
+        : "libctgui-ext-x86_64.dylib";
+
+    case "windows":
+      return "ctgui-ext.dll";
+
+    default:
+      throw new Error("Unsupported OS: " + Deno.build.os);
+  }
+}
+
+const res = await fetch(
+  `https://github.com/diyorbek/tgui.js/releases/download/v${VERSION.join(
+    "."
+  )}/${getBinaryName()}`
+);
+
+Deno.writeFileSync("libctgui", await res.bytes());
